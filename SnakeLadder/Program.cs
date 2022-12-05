9ec3aed17217759568d4b37859aa26fa0197429b
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-05 21:56:08 +0530

    Game played with single player

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+            
+            Snake snake = new Snake();
+            snake.Ladder();
+
+
+        }
+    }
+}
+        
+    
+
