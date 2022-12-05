701b7970e7b914bf61406b26cd9ba89734460ff9
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-05 23:01:29 +0530

    Report the number of times the dice was played to win the game

---
+++ DiceRollCount/DieRoll.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollCount
+{
+    public class DieRoll
+    {
+        public const int ladder = 1, snake = 2;
+        public void Ladder()
+        {
+            int position1 = 0;
+            int dice = 0;
+            int roll = 0;
+            Console.WriteLine("The starting position of player is : " + position1);
+            Console.WriteLine(" ");
+            do
+            {
+                roll++;
+                Random random = new Random();
+                dice = random.Next(1, 7);
+                Console.WriteLine("The number got by dice roll is : " + dice);
+                Random random2 = new Random();
+                int option = random2.Next(0, 3);
+                switch (option)
+                {
+                    case ladder:
+                        position1 += dice;
+                        Console.WriteLine("The player climbed the ladder");
+                        break;
+                    case snake:
+                        position1 -= dice;
+                        if (position1 < 0)
+                        {
+                            Console.WriteLine("The player bit by snake");
+                            position1 = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The player bit by snake");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("No play");
+                        break;
+                }
+                if (position1 > 100)
+                {
+                    position1 = 0;
+                }
+
+                Console.WriteLine("The player position is : " + position1);
+                Console.WriteLine("The dice has been rolled " + roll + " times till now");
+                Console.WriteLine(" ");
+            } while (position1 < 100);
+            Console.WriteLine($"The dice has been rolled " + roll + " times to reach 100");
+        }
+    }
+}
+    
+
+
---
+++ DiceRollCount/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+namespace DiceRollCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DieRoll dieRoll = new DieRoll();
+            dieRoll.Ladder();
+
+        }
+    }
+}
