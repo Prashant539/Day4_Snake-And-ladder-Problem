941e368ffbec26abe248a85247237c63f86b3c7e
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-05 23:10:53 +0530

    Play the game with 2 player

---
+++ TwoPlayerGAme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TwoPlayerGAme;
+
+namespace TwoPlayerGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayerWinGAme playerWinGAme = new PlayerWinGAme();
+            playerWinGAme.Ladder();
+
+        }
+            
+
+
+
+    }
+        
+}
+
+
+
