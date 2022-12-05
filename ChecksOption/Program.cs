107c158ab99e73e41b0b49ceefdac5f1cb93900c
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-05 22:28:09 +0530

    Check for option

---
+++ ChecksOption/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace ChecksOption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayOption playOption = new PlayOption();
+            playOption.Ladder();
+
+           
+        }
+    }
+}
